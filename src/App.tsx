64dceec5405003c6a32ee4adf11476368a90e47b
George <71167099+letow@users.noreply.github.com>
2023-11-20 03:04:14 +0300

    added search

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,89 @@
 import "./App.scss";
 import "primeflex/primeflex.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
-import { PrimeReactProvider } from "primereact/api";
+import { FilterMatchMode, PrimeReactProvider } from "primereact/api";
 import { TabView, TabPanel } from "primereact/tabview";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Table from "./components/Table/Table";
 import { observer } from "mobx-react-lite";
 import { DataView } from "primereact/dataview";
-import card from "./store/Cards";
+import card from "./store/Events";
 import CardItem from "./components/CardItem/CardItem";
+import { Message } from "primereact/message";
+import { InputText } from "primereact/inputtext";
+import { Panel } from "primereact/panel";
+import { Button } from "primereact/button";
 
 const App = observer(() => {
     useEffect(() => {
         card.getEvents();
 
-        const handleKeydown = (e: any) => {
+        const handleKeydown = (e: KeyboardEvent) => {
             if (e.code === "Space" && card.activeCard) {
                 e.preventDefault();
                 card.readEvent(card.activeCard);
             }
         };
+
         document.addEventListener("keydown", handleKeydown);
 
         return () => document.removeEventListener("keydown", handleKeydown);
     }, []);
 
+    const [filters, setFilters] = useState({
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    });
+
+    const [globalFilterValue, setGlobalFilterValue] = useState<string>("");
+
+    const onGlobalFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setGlobalFilterValue(e.target.value);
+    };
+
+    const onSearch = () => {
+        const value = globalFilterValue;
+        let _filters = { ...filters };
+
+        _filters["global"].value = value;
+
+        setFilters(_filters);
+    };
+
+    const renderHeader = () => {
+        return (
+            <div className="flex justify-content-start z-5 w-full static md:absolute md:justify-content-end">
+                <InputText
+                    value={globalFilterValue}
+                    onChange={onGlobalFilterChange}
+                    placeholder="Введите запрос..."
+                />
+                <Button className="bg-primary ml-2" onClick={onSearch}>
+                    Поиск
+                </Button>
+            </div>
+        );
+    };
+
+    const header = renderHeader();
+
     return (
         <PrimeReactProvider>
             <div className="App">
-                <div className="surface-0">
+                <Panel headerTemplate={header} className="relative">
+                    {card.isError && <Message severity="error" text={card.error} />}
                     <TabView>
                         <TabPanel header="Таблица">
-                            <Table events={card.events} />
+                            <Table
+                                events={card.events}
+                                filters={filters}
+                                globalFilterFields={[
+                                    "date",
+                                    "importance",
+                                    "ware",
+                                    "message",
+                                    "responsible",
+                                ]}
+                            />
                         </TabPanel>
                         <TabPanel header="Карточки">
                             <DataView
@@ -43,7 +95,7 @@ const App = observer(() => {
                             />
                         </TabPanel>
                     </TabView>
-                </div>
+                </Panel>
             </div>
         </PrimeReactProvider>
     );
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,13 +1,15 @@
 import { Column } from "primereact/column";
-import { DataTable } from "primereact/datatable";
+import { DataTable, DataTableFilterMeta } from "primereact/datatable";
 import { FC } from "react";
 import { Event_ } from "../../types/Event_";
 
 interface TableProps {
     events: Event_[];
+    globalFilterFields: string[];
+    filters: DataTableFilterMeta;
 }
 
-const Table: FC<TableProps> = ({ events }) => {
+const Table: FC<TableProps> = ({ events, globalFilterFields, filters }) => {
     return (
         <DataTable
             value={events}
@@ -16,13 +18,20 @@ const Table: FC<TableProps> = ({ events }) => {
             stripedRows
             paginator
             rows={10}
+            filters={filters}
             tableStyle={{ minWidth: "10rem" }}
+            globalFilterFields={globalFilterFields}
         >
-            <Column field="date" header="Дата"></Column>
-            <Column field="importance" header="Важность"></Column>
-            <Column field="ware" header="Оборудование"></Column>
-            <Column field="message" header="Сообщения" sortable></Column>
-            <Column field="responsible" header="Ответственный" sortable></Column>
+            <Column field="date" header="Дата" filterField="date"></Column>
+            <Column field="importance" header="Важность" filterField="importance"></Column>
+            <Column field="ware" header="Оборудование" filterField="ware"></Column>
+            <Column field="message" header="Сообщения" filterField="message" sortable></Column>
+            <Column
+                field="responsible"
+                header="Ответственный"
+                filterField="responsible"
+                sortable
+            ></Column>
         </DataTable>
     );
 };
